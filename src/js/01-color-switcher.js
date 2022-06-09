e7c4a1e0352ffba0c513574b7d42845ba45c7276
Volodymyr Bobanych <97664943+VollDeMarr@users.noreply.github.com>
2022-06-09 22:27:08 +0300

    super bg-color with gradient

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let interval = null;
+let upperInterval = null;
 
+btnStart.addEventListener('click', () => {
+  btnStart.setAttribute('disabled', true);
+  interval = setInterval(() => {
+    body.style.backgroundImage = `linear-gradient(to right,
+        ${getRandomHexColor()},
+        ${getRandomHexColor()}`;
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(interval);
+  btnStart.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
